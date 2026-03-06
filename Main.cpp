fa92ece53f8b58758d56d9a83a4205b062238788
moglitch249 <moglitch249@users.noreply.github.com>
2026-03-06 15:23:16 -0500

    main

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,21 @@
 #include <SFML/Graphics.hpp>
+using namespace sf;
+
+int main() 
+{
+    RenderWindow window(VideoMode({800, 600}), "My Window");
+
+    while (window.isOpen())
+    {
+        while (const auto event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
+}
