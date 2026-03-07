27849aaca970d8a0196a1a7a87572984b5e76cb8
moglitch249 <moglitch249@users.noreply.github.com>
2026-03-07 10:34:48 -0500

    update

--- gameglobale.h
+++ gameglobale.h
@@ -4,4 +4,12 @@
   all global variable declaration
   -------------------------------
 */
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <vector>
+using namespace sf;
+
+   const int WINDOW_WIDTH = 1600;
+   const int WINDOW_HEIGHT = 900;
+   int score = 0;
 #endif
