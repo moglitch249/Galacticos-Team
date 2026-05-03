3bf4e434d525878e9f53c43b603f2f66fa2a0531
Alint7ary <alimagdi8887@gmail.com>
2026-05-04 01:13:17 +0300

    Add files via upload

--- Physics.h
+++ Physics.h
@@ -4,3 +4,4 @@
 void physicsUpdate(Player& p, float dt);
 void resolvePlayerCollision(Player& a, Player& b);
 void handleCombat(Player& attacker, Player& defender);
+void resolvePlatformCollision(Player& p, const Platform& plat);
